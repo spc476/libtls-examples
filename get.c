85e0c3b7f83ac1419cd5be9b2ac6902743b7f851
Sean Conner <spc@conman.org>
2018-07-18 22:43:53 -0400

    A working example of GET.

--- get.c
+++ get.c
@@ -26,9 +26,30 @@
 
 #include <tls.h>
 
+struct tls_config *g_config = NULL;
+struct tls        *g_tls    = NULL;
+
+/***********************************************************************/
+
+void cleanup(void)
+{
+  if (g_tls)
+  {
+    tls_close(g_tls);
+    tls_free(g_tls);
+  }
+  
+  if (g_config) tls_config_free(g_config);
+}
+
+/***********************************************************************/
+
 int main(int argc,char *argv[])
 {
-  struct tls_config *config;
+  char  buffer[BUFSIZ];
+  char *p;
+  int   rc;
+  int   writebytes;
   
   if (argc == 1)
   {
@@ -36,19 +57,100 @@ int main(int argc,char *argv[])
     return EXIT_FAILURE;
   }
   
+  atexit(cleanup);
+  
   if (tls_init() < 0)
   {
     perror("tls_init()");
     return EXIT_FAILURE;
   }
   
-  config = tls_config_new();
-  if (config == NULL)
+  g_config = tls_config_new();
+  if (g_config == NULL)
   {
     perror("tls_config()");
     return EXIT_FAILURE;
   }
   
-  tls_config_free(config);
+  rc = tls_config_set_protocols(g_config,TLS_PROTOCOLS_ALL);
+  if (rc != 0)
+  {
+    perror(tls_config_error(g_config));
+    return EXIT_FAILURE;
+  }
+  
+  g_tls = tls_client();
+  if (g_tls == NULL)
+  {
+    perror("tls_client()");
+    return EXIT_FAILURE;
+  }
+  
+  rc = tls_configure(g_tls,g_config);
+  if (rc != 0)
+  {
+    perror(tls_error(g_tls));
+    return EXIT_FAILURE;
+  }
+  
+  rc = tls_connect(g_tls,argv[1],"https");
+  if (rc != 0)
+  {
+    perror(tls_error(g_tls));
+    return EXIT_FAILURE;
+  }
+  
+  writebytes = snprintf(
+            buffer,
+            sizeof(buffer),
+            "GET %s HTTP/1.1\r\n"
+            "Host: %s\r\n"
+            "User-Agent: TLSTester/1.0 (TLS Testing Program)\r\n"
+            "Connection: close\r\n"
+            "Accept: */*\r\n"
+            "\r\n",
+            argv[2],
+            argv[1]
+          );
+  if ((writebytes < 0) || ((unsigned)writebytes > sizeof(buffer)))
+  {
+    fprintf(stderr,"snprintf() error\n");
+    return EXIT_FAILURE;
+  }
+  
+  p = buffer;
+  
+  while(writebytes > 0)
+  {
+    ssize_t outbytes = tls_write(g_tls,p,(size_t)writebytes);
+    if ((outbytes == TLS_WANT_POLLIN) || (outbytes == TLS_WANT_POLLOUT))
+      continue;
+    if (outbytes < 0)
+    {
+      perror(tls_error(g_tls));
+      return EXIT_FAILURE;
+    }
+    
+    writebytes -= outbytes;
+    p          += outbytes;
+  }
+  
+  while(1)
+  {
+    ssize_t inbytes = tls_read(g_tls,buffer,sizeof(buffer));
+    if ((inbytes == TLS_WANT_POLLIN) || (inbytes == TLS_WANT_POLLOUT))
+      continue;
+    if (inbytes < 0)
+    {
+      perror(tls_error(g_tls));
+      return EXIT_FAILURE;
+    }
+    
+    if (inbytes == 0) break;
+    fwrite(buffer,1,(size_t)inbytes,stdout);
+  }
+  
   return EXIT_SUCCESS;
 }
+
+/***********************************************************************/
